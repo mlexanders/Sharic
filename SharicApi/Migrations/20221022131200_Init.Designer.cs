1f3b2156e8b7b7beda8c2225001e3bd9cf431e19
mlexanders <mlexanders@outlook.com>
2022-10-22 16:26:25 +0300

    Add Dbsets

--- SharicApi/Migrations/20221022131200_Init.Designer.cs
+++ SharicApi/Migrations/20221022131200_Init.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SharicApi;
 
@@ -10,9 +11,10 @@ using SharicApi;
 namespace SharicApi.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221022131200_Init")]
+    partial class Init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.10");
--- SharicApi/Program.cs
+++ SharicApi/Program.cs
@@ -18,6 +18,9 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(connectionString);
 });
 
+
+//builder.Services.AddTransient<>
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
