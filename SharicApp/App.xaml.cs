899c1f9757ed54f03ec5225861e0ea85378b47f2
mlexanders <mlexanders@outlook.com>
2022-10-22 03:26:06 +0300

    Добавьте файлы проекта.

---
+++ SharicApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SharicApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ SharicApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SharicApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
