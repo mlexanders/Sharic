f30bfd70b4676b659a7c423116351accc87a745c
Салтыков Виктор <saltyckov.vitya@yandex.ru>
2022-10-22 15:43:48 +0300

    Add authorization

---
+++ SharicApi/AppSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace SharicApi
+{
+    public class AppSettings
+    {
+        public Jwt? Jwt { get; set; }
+        public string? StaticToken { get; set; }
+    }
+    public class Jwt
+    {
+        public string? Key { get; set; }
+        public string? Issuer { get; set; }
+        public string? Audience { get; set; }
+        public uint Lifetime { get; set; }
+    }
+}
--- SharicApi/Controllers/User.cs
+++ SharicApi/Controllers/User.cs
@@ -7,5 +7,7 @@ namespace SharicApi.Controllers
         public int Id { get; set; }
         public string? Token { get; set; }
         public Role Role { get; set; }
+        public string Login { get; set; }
+        public Password Password { get; set; }
     }
 }
