0f15ece57ef86d3e391dbef223446cd5536a23cb
Eva <evapirozhok@gmail.com>
2022-10-22 15:22:42 +0300

    add models Roads/Trip/Points

---
+++ SharicCommon/Data/Models/Roads.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharicCommon.Data.Models
+{
+    public class Road
+    {
+        public int Road_Id { get; set; }
+        public int Source_Point_Id { get; set; }
+        public int Target_Point_Id { get; set; }
+        public int Distance { get; set; }
+    }
+}
