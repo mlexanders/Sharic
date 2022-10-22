a1c32428cb07a00330de3b922f7ff32ac7be533c
Eva <evapirozhok@gmail.com>
2022-10-22 15:52:36 +0300

    add Model Buses

---
+++ SharicCommon/Data/Models/Buses.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharicCommon.Data.Models
+{
+   public class Bus
+    {
+        public int BusId { get; set; }
+        public int PeopleIn { get; set; }
+        public int Speed { get; set; }
+    }
+}
