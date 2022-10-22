f8fc3c0ca2ff5946dc453474418bcd4d0282c25f
Eva <evapirozhok@gmail.com>
2022-10-22 15:43:11 +0300

    add Model TaskState

---
+++ SharicCommon/Data/Models/TaskState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharicCommon.Data.Models
+{
+    public class TaskState
+    {
+        public DateTime Time_Start { get; set; }
+        public DateTime Time_Onboard { get; set; }
+        public DateTime Time_UnBoard { get; set; }
+        public DateTime Time_End { get; set; }
+    }
+}
